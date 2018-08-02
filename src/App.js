c814a52ea8e7226807e1ce1823e87531579485f2
CecileJiaqiWang <jw81@rice.edu>
2018-08-02 18:37:25 -0500

    create main section for registration form

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import './App.css';
-import {Header} from "./Header";
-
+import {Header} from './Header';
+import {Main} from './Main';
 class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <Header/>
-        <p className="App-intro">
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Header/>
+                <Main/>
+
+            </div>
+        );
+    }
 }
 
 export default App;
