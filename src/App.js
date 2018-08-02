448fd551be02f90aa77bfcddd20e91aef9309ea9
CecileJiaqiWang <jw81@rice.edu>
2018-08-02 18:29:20 -0500

    header style

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ class App extends React.Component {
       <div className="App">
         <Header/>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
