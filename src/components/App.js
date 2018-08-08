cd91ff5800d4211f4fccb8ca3286b9b4567ecd90
CecileJiaqiWang <jw81@rice.edu>
2018-08-08 02:28:53 -0500

    combines routers

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,32 @@
 import React from 'react';
-import '../styles/App.css';
 import {Header} from './Header';
 import {Main} from './Main';
+import {TOKEN_KEY} from '../Constants';
+import '../styles/App.css';
+
 class App extends React.Component {
+    state = {
+        // Handles null
+        isLoggedIn: !!localStorage.getItem(TOKEN_KEY),
+    }
+
+    handleLogin = (response) => {
+        localStorage.setItem(TOKEN_KEY, response);
+        this.setState({isLoggedIn: true});
+    }
+
+    handleLogout = () => {
+        localStorage.removeItem(TOKEN_KEY);
+        this.setState({isLoggedIn: false});
+    }
+
     render() {
         return (
             <div className="App">
-                <Header/>
-                <Main/>
+                <Header isLoggedIn={this.state.isLoggedIn}
+                        handleLogout={this.handleLogout}/>
+                <Main isLoggedIn={this.state.isLoggedIn}
+                      handleLogin={this.handleLogin}/>
 
             </div>
         );
