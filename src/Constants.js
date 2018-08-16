44b69b4a833d0f125c6a00179fdca5d4871caee0
CecileJiaqiWang <jw81@rice.edu>
2018-08-15 20:25:10 -0500

    be able to get geolocations and handle errors

--- src/Constants.js
+++ src/Constants.js
@@ -1,2 +1,8 @@
 export const API_ROOT = 'https://around-75015.appspot.com/api/v1';
 export const TOKEN_KEY = 'TOKEN_KEY';
+export const POSITION_KEY = 'POSITION_KEY';
+export const GEO_OPTIONS = {
+    enableHighAccuracy: true,
+    maximumAge: 30000,
+    timeout: 27000,
+}
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Tabs, Button } from 'antd';
+import {POSITION_KEY, GEO_OPTIONS} from "../Constants"
 
 
 const TabPane = Tabs.TabPane;
@@ -7,6 +8,50 @@ const TabPane = Tabs.TabPane;
 const operations = <Button>Extra Action</Button>;
 
 export class Home extends React.Component {
+    state = {
+        loadingGeoLocation: false,
+        loadingPosts: false,
+        error: '',
+        posts: [],
+    }
+
+    componenentDidMount() {
+        this.setState({loadingGeoLocation: true, error: ''});
+        this.getGeoLocation();
+    }
+
+    getGeoLocation = () => {
+        if ('geoLocation' in navigator) {
+            navigator.geoLocation.getCurrentPosition(
+                this.onSuccessLoadGeoLocation,
+                this.onFailedLoadGeolocation,
+                GEO_OPTIONS,
+            )
+        }
+    }
+
+    onSuccessLoadGeoLocation = (position) => {
+        console.log(position);
+        this.setState({loadingGeoLocation: false, error: ''});
+        // Destruct.
+        const{ latitude, longitude } = position.coords;
+        localStorage.setItem(POSITION_KEY, JSON .stringify({lat: latitude, lon: longitude}));
+        // ToDo: Load Nearby Posts according the current location.
+
+    }
+
+    onFailedLoadGeolocation = () => {
+        this.setState({loadingGeoLocation: false, error: 'Failed to load geolocation!'});
+    }
+
+
+
+
+
+
+
+
+
     render() {
         return (
             <Tabs tabBarExtraContent={operations}>
